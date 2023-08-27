989aa89db0c1cbaa145ee932e33110213923dda6
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-27 15:06:06 -0400

    update helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -4,4 +4,14 @@ def clear_screen():
     """Clears the terminal screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
 
- 
+def get_positive_integer_input(prompt):
+      """Prompts the user for a positive integer input."""
+      while True:
+        try:
+            value = int(input(prompt))
+            if value <= 0:
+                print("Please enter a positive integer.")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input. Please enter a valid positive integer.")
