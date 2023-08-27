74792a52f49fb2da7dbb6841d993dff4fb8f4bdc
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-27 15:01:26 -0400

    update seed.py

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -10,4 +10,19 @@ session = Session()
 
 Base.metadata.create_all(engine) 
 
- 
+ # Products
+iphone = Product(name="Galaxy 23", price=899)
+macbook = Product(name="MacBook Pro 16", price=2399) 
+
+# Suppliers            
+apple = Supplier(name="Apple Inc.", email="sales@apple.com")
+samsung = Supplier(name="Samsung Electronics", email="sales@samsung.com")
+
+# Employees
+john = Employee(name="John Smith", role="Sales Manager")
+jane = Employee(name="Jane Lee", role="Sales Representative")
+
+session.add_all([iphone, macbook, apple, samsung, john, jane])
+session.commit()
+
+print("Seeded database successfully")
