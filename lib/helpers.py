bdd4e3d714ad8d1a7bf19bb62184189eb1a51267
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-27 21:48:11 -0400

    create function1

--- lib/helpers.py
+++ lib/helpers.py
@@ -30,4 +30,23 @@ def get_positive_integer_input(prompt):
         except ValueError:
             print("Invalid input. Please enter a valid positive integer")
 
- 
+def add_product(name, quantity_range, supplier_id):
+    quantity_range = Range(quantity_range[0], quantity_range[1])
+    new_product = Product(name=name, quantity=quantity_range, supplier_id=supplier_id)
+    session.add(new_product)
+    session.commit()
+def list_products():
+    products = session.query(Product).all()
+    results = [Tuple(product.id, product.name, product.quantity) for product in products]
+    print(f"Found {len(results)} products:")
+    for id, name, quantity in results:
+        print(f"{id}: {name} - {quantity}")
+def add_supplier(name, products):
+    products = [Tuple(p[0], p[1]) for p in products]
+    new_supplier = Supplier(name=name, products=products)
+    session.add(new_supplier)
+    session.commit()
+def assign_task(employee_id, description):
+    new_task = Task(employee_id=employee_id, description=description)
+    session.add(new_task)
+    session.commit()
