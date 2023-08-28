b26f34f146bc321eadd237237acda2a84614d339
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-27 21:46:27 -0400

    update helper function

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,12 +1,26 @@
 import os
+from collections import namedtuple
+from lib.db.models import Product, Supplier, Task
+from lib.db import session
+from ranges import Range
+import tuples
 
+# Clear the terminal screen
 def clear_screen():
-    """Clears the terminal screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
 
+# Custom Tuple class
+Tuple = namedtuple('Tuple', ['x', 'y'])
+
+# Custom Range class
+class Range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+# Function to get a positive integer input
 def get_positive_integer_input(prompt):
-      """Prompts the user for a positive integer input."""
-      while True:
+    while True:
         try:
             value = int(input(prompt))
             if value <= 0:
@@ -14,4 +28,6 @@ def get_positive_integer_input(prompt):
             else:
                 return value
         except ValueError:
-            print("Invalid input. Please enter a valid positive integer.")
+            print("Invalid input. Please enter a valid positive integer")
+
+ 
