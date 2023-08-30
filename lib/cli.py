1dcce0f63d7e9857f5898281217fe8ad2b99b32f
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-30 18:44:01 -0400

    update files

--- lib/cli.py
+++ lib/cli.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from lib.db.helpers import (
+from lib.helpers import (
     confirm_and_add_product, list_products,
     confirm_and_add_supplier, confirm_and_assign_task,
     clear_screen, get_positive_integer_input
@@ -23,52 +23,4 @@ def main_menu(self):
             print("4. Assign Task")
             print("5. Exit")
 
-# lib/db/helpers.py
-
-from lib.db.models import Product, Supplier, Task
-from lib.db import session
-from ranges import Range
-import tuples
-
-def add_product(name, quantity_range, supplier_id):
-  """Add a new product to the database"""
-
-  # Use range for quantity
-  quantity_range = Range.from_tuple(quantity_range)
-
-  new_product = Product(name=name, quantity=quantity_range, supplier_id=supplier_id)
-
-  session.add(new_product)
-  session.commit()
-
-def list_products():
-  """List all products in the database"""
-
-  # Use tuple for query results
-  products = session.query(Product).all()
-
-  results = [(product.id, product.name, product.quantity) for product in products]
-
-  print(f"Found {len(results)} products:")
-
-  for id, name, quantity in results:
-    print(f"{id}: {name} - {quantity}")
-
-def add_supplier(name, products):
-  """Add a new supplier to the database"""
-
-  # Tuple for initial products
-  products = tuples.from_list(products)
-
-  new_supplier = Supplier(name=name, products=products)
-
-  session.add(new_supplier)
-  session.commit()
-
-def assign_task(employee_id, description):
-  """Assign a new task to an employee"""
-
-  new_task = Task(employee_id=employee_id, description=description)
-
-  session.add(new_task)
-  session.commit()
+ 
--- lib/db/models.py
+++ lib/db/models.py
@@ -1,58 +1,75 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base() 
+Base = declarative_base()
+
 
 class Product(Base):
     __tablename__ = 'products'
-
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
-    price = Column(Integer)
+    name = Column(String, nullable=False)
+    inventory_quantity = Column(Integer, default=0)
+    shipments = relationship('Shipment', back_populates='product')
 
-    supplier_id = Column(Integer, ForeignKey('suppliers.id'))
+    def __init__(self, name, inventory_quantity):
+        self.name = name
+        self.inventory_quantity = inventory_quantity
 
- 
+
 class Supplier(Base):
     __tablename__ = 'suppliers'
-
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    location = Column(String)
+    name = Column(String, nullable=False)
+    products = relationship('Product', secondary='supplier_product')
 
-    products = relationship('Product', backref='supplier')
+    def __init__(self, name):
+        self.name = name
+
+
+supplier_product = Table(
+    'supplier_product',
+    Base.metadata,
+    Column('supplier_id', Integer, ForeignKey('suppliers.id')),
+    Column('product_id', Integer, ForeignKey('products.id'))
+)
+
 
 class Employee(Base):
     __tablename__ = 'employees'
-
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    department = Column(String) 
- 
-class Shipment(Base):
-    __tablename__ = 'shipments'
+    name = Column(String, nullable=False)
+    tasks = relationship('Task', order_by='Task.id', back_populates='employee')
 
-    id = Column(Integer, primary_key=True)
-    shipping_date = Column(String)
-    delivery_date = Column(String)
+    def __init__(self, name):
+        self.name = name
 
-    employees = relationship('Employee', secondary='shipment_employees')
-
-class ShipmentEmployee(Base):
-    __tablename__ = 'shipment_employees'
-
-    id = Column(Integer, primary_key=True)
-    shipment_id = Column(Integer, ForeignKey('shipments.id'))
-    employee_id = Column(Integer, ForeignKey('employees.id'))
 
 class Task(Base):
     __tablename__ = 'tasks'
-
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    assignee_id = Column(Integer, ForeignKey('employees.id'))
+    description = Column(String, nullable=False)
+    employee_id = Column(Integer, ForeignKey('employees.id'))
+    employee = relationship('Employee', back_populates='tasks')
 
-    assignee = relationship('Employee')
-    
+    def __init__(self, description, employee):
+        self.description = description
+        self.employee = employee
+
+
+class Shipment(Base):
+    __tablename__ = 'shipments'
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey('products.id'))
+    product = relationship('Product', back_populates='shipments')
+    quantity = Column(Integer, nullable=False)
+
+    def __init__(self, product, quantity):
+        self.product = product
+        self.quantity = quantity
+
+
+engine = create_engine('sqlite:///lib/db/warehouse.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
